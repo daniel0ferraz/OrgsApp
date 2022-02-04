ac1146586eae97c921d53b294a2ccc43f83efd5e
Daniel Ferraz <Daniel0ferraz@outlook.com>
2022-02-04 10:56:28 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import Carrinho from 'pages/Carrinho';
+import Feira from 'pages/Feira';
+import Login from 'pages/Login';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { UsuarioProvider } from 'common/context/Usuario';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <UsuarioProvider>
+          <Route exact path="/">
+            <Login />
+          </Route>
+
+          <Route path="/feira">
+            <Feira />
+          </Route>
+        </UsuarioProvider>
+
+        <Route path="/carrinho">
+          <Carrinho />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
